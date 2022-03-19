e2e9c2b78813a6b0ad37e4f28b69f003e77488ce
Luiz Miguel dos Santos Gonçalves <lm4academy@gmail.com>
2022-03-19 11:13:25 -0300

    login_data added

--- login_data.js
+++ login_data.js
@@ -3,7 +3,7 @@ let uri_s
 
 try{
     const login = require("./login")
-    uri_s = `mongodb+srv://${login.user_name}:${login.password}@cluster0.hbyb1.mongodb.net/${login.db_name}?retryWrites=true&w=majority`
+    uri_s = `mongodb+srv://${login.user_name}:${login.password}@${login.cluster_name}.mongodb.net/${login.db_name}?retryWrites=true&w=majority`
 }catch{
     uri_s = process.env.MONGODB_URI
 }
