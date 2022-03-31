dc8579a9b6a09f69395ea514d9dffdd23479366a
Luiz Miguel dos Santos Gonçalves <lm4academy@gmail.com>
2022-03-31 13:06:32 -0300

    mongo done added and json creator show options button removed

--- db/mongo.js
+++ db/mongo.js
@@ -1,30 +1,44 @@
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const ObjectId = require("mongodb").ObjectId
 const login = require("./login_mongo")
-const BSONTypeError = require("mongodb").BSONTypeError
 
 const uri = login.uri
+let done = false
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
 client.connect(err => {
-    global.conn = client.db(login.db_name)
+    if (!err){
+        done = true
+        global.conn = client.db(login.db_name)
+        return
+    }
+    console.log("Houve um erro na conexão com o banco de dados mango_db: "+ err)
 });
 
 async function selectTitles() {
+    if (!done){
+        return []
+    }
     let values = await global.conn.collection(login.collection_name).find({}, {projection: {Titulo: true}}).toArray()
     
     return values
 }
 
 async function getMyTitles(user_id){
+    if (!done){
+        return []
+    }
     let values = await global.conn.collection(login.collection_name).find({owner: {$eq: user_id}}, {projection: {Titulo: true}}).toArray()
 
     return values
 }
 
 async function loadHistory(id) {
+    if (!done){
+        return {}
+    }
     try{
         id = ObjectId(id)
-    }catch(BSONTypeError){
+    }catch(err){
         id = ""
     }
     let value = await global.conn.collection(login.collection_name).findOne({_id: id}, {projection: {_id: false}})
