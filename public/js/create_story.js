2e6a00ba8443b47cfec467b37a6e06f53d150b99
Ana <anabhering1@hotmail.com>
2022-05-08 21:26:13 -0300

    prototipo home

--- public/js/create_story.js
+++ public/js/create_story.js
@@ -1,65 +1,65 @@
-function show_options(){
+function show_options() {
     let input_number = this
-    if (input_number.value==''){
+    if (input_number.value == '') {
         input_number.value = 0
     }
 
-    if(input_number.value > Number(input_number.max)){
+    if (input_number.value > Number(input_number.max)) {
         input_number.value = 5
-        return 
+        return
     }
-    if (input_number.value < 0){
+    if (input_number.value < 0) {
         input_number.value = 0
-        return 
+        return
     }
 
-    let i=0
+    let i = 0
     let c_num = input_number.id.split("_")[2]
 
-    let option = document.getElementById(c_num+"_option_"+i.toString())
-    while (option !== null){
+    let option = document.getElementById(c_num + "_option_" + i.toString())
+    while (option !== null) {
         i++
-        option = document.getElementById(c_num + "_option_"+i.toString())
+        option = document.getElementById(c_num + "_option_" + i.toString())
     }
 
     let table = document.getElementById("option_list_" + c_num)
 
-    if(input_number.value > i){
-        for (;i<input_number.value;i++){
+    if (input_number.value > i) {
+        for (; i < input_number.value; i++) {
             let el = document.createElement('div')
             el.className = 'mb-3 form_item'
             let el2 = document.createElement('div')
             el2.className = 'mb-3 form_item'
-            el.id = c_num + "_option_"+i.toString()
-            el2.id = c_num + "_option_link_"+i.toString()
-    
+            el.id = c_num + "_option_" + i.toString()
+            el2.id = c_num + "_option_link_" + i.toString()
+
             el.innerHTML = `
                         <label>Opção ${i}: </label> 
                         <textarea rows="3" style="width:100%;" id="${c_num}_option_number_${i}">`
-                        el2.innerHTML = `
+            el2.innerHTML = `
                         <label>Link ${i}: </label> 
                         <input type="number" value="-1" id="${c_num}_option_input_link_${i}" style="width:80px;">
                         `
-                        
+
             table.appendChild(el)
             table.appendChild(el2)
         }
-    }else{
-        for (;i>input_number.value;i--){
-            option = document.getElementById(c_num+"_option_"+(i-1).toString())
-            link = document.getElementById(c_num + "_option_link_"+(i-1).toString())
+    } else {
+        for (; i > input_number.value; i--) {
+            option = document.getElementById(c_num + "_option_" + (i - 1).toString())
+            link = document.getElementById(c_num + "_option_link_" + (i - 1).toString())
             option.remove()
             link.remove()
         }
     }
 
-    
+
 }
 
 function add_choice() {
     let new_choice = document.createElement("div")
     new_choice.className = "input_atom"
-    new_choice.style.maxWidth = '600px'
+    new_choice.style.maxWidth = '900px'
     console.log(actual_id)
 
     new_choice.innerHTML = `
@@ -107,57 +107,57 @@ function add_choice() {
 
 }
 
-function add_option(){
+function add_option() {
     let c_num = this.id.split("_")[2]
     quant_options = document.getElementById(`input_number_${c_num}`)
-    quant_options.value ++
-    quant_options.dispatchEvent(new Event("change"))
+    quant_options.value++
+        quant_options.dispatchEvent(new Event("change"))
 }
 
-function sub_option(){
+function sub_option() {
     let c_num = this.id.split("_")[2]
     quant_options = document.getElementById(`input_number_${c_num}`)
-    quant_options.value --
-    quant_options.dispatchEvent(new Event("change"))
+    quant_options.value--
+        quant_options.dispatchEvent(new Event("change"))
 }
 
-function generate_json(){
+function generate_json() {
     json_result = {
         "Titulo": document.getElementById('title').value,
         "Escolhas": []
     }
 
-    let i=0
+    let i = 0
 
     choice = document.getElementById(`choice_table_${i}`)
 
-    for (;choice !== null;){
+    for (; choice !== null;) {
         choice_json = {
             "id": i,
             "História": document.getElementById(`history_${i}`).value,
             "Opções": []
         }
-        
-        let j=0
 
-        let option = document.getElementById(i.toString()+"_option_number_"+j.toString())
-        let link = document.getElementById(i.toString()+"_option_input_link_"+j.toString())
-        while (option !== null){
-            let option_json = {"Id": j, "Texto": option.value, "link": null}
+        let j = 0
 
-            if(link.value ==  ''){
-                option_json.link  = null
+        let option = document.getElementById(i.toString() + "_option_number_" + j.toString())
+        let link = document.getElementById(i.toString() + "_option_input_link_" + j.toString())
+        while (option !== null) {
+            let option_json = { "Id": j, "Texto": option.value, "link": null }
+
+            if (link.value == '') {
+                option_json.link = null
             }
-            
-            if (link.value >= 0 && link.value < actual_id){
+
+            if (link.value >= 0 && link.value < actual_id) {
                 option_json.link = Number.parseInt(link.value)
             }
 
             choice_json["Opções"].push(option_json)
 
             j++
-            option = document.getElementById(i.toString()+"_option_number_"+j.toString())
-            link = document.getElementById(i.toString()+"_option_input_link_"+j.toString())
+            option = document.getElementById(i.toString() + "_option_number_" + j.toString())
+            link = document.getElementById(i.toString() + "_option_input_link_" + j.toString())
         }
 
         json_result['Escolhas'].push(choice_json)
@@ -169,10 +169,10 @@ function generate_json(){
     handle_json(json_result)
 }
 
-function deleteLastChoice(){
+function deleteLastChoice() {
     choices = document.getElementsByClassName('input_atom')
-    if(actual_id>1){
-        choices[choices.length-1].remove()
+    if (actual_id > 1) {
+        choices[choices.length - 1].remove()
         actual_id--
     }
 }
